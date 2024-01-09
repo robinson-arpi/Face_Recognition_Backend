12e7445106299b5a9eb3a81d7e96a8b79d22222e
robinson-arpi <robinson.arpi@gmail.com>
2024-01-09 00:15:13 -0500

    realtime transmition

---
+++ static/js/main.js
@@ -0,0 +1,27 @@
+// main.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtener los elementos de los cards
+    const trainingCard = document.getElementById('trainingCard');
+    const uploadCard = document.getElementById('uploadCard');
+    const realtimeCard = document.getElementById('realtimeCard');
+
+    // Función para redirigir a la página especificada
+    function redirectToPage(page) {
+        window.location.href = `/${page}`;
+    }
+
+    // Agregar manejadores de eventos de clic a los cards
+    trainingCard.addEventListener('click', function () {
+        redirectToPage('training');
+    });
+
+    uploadCard.addEventListener('click', function () {
+        redirectToPage('upload');
+    });
+
+    realtimeCard.addEventListener('click', function () {
+        redirectToPage('realtime');
+        
+    });
+});
